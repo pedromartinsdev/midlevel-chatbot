38605dfe5eccb68fd8c2799da0b9c84b6ef0c4db
Pedro <pedrohenriquemartins@live.com>
2025-03-18 22:30:18 -0300

    configure mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,24 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 const app = express()
-const port = 3000
-
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT
+const MONGO_URI = process.env.MONGO_URI
 
 app.get('/', (req, res) => {
   res.send("Hello World!")
   console.log(dotenv)
 })
 
-app.listen(port, () => {
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('Conectado ao MongoDB'))
+  .catch((err) => console.error('Erro na conexão com o Atlas', err))
+
+app.listen(PORT, () => {
   console.log("Listening...")
 })
